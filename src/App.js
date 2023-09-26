5361aa31a2d7aa6ee7ae9db9be8fcb4f1e7b1a3b
wendeldevilla <devillawendel2731@gmail.com>
2023-09-27 04:04:59 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./Components/Homepage";
+import AnimeItem from "./Components/AnimeItem";
+import Gallery from "./Components/Gallery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/anime/:id" element={<AnimeItem />} />
+        <Route path="/character/:id" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Globalstyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Handjet&family=Inter&family=Vina+Sans&display=swap');
+  
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        font-family: 'Inter' , sans-serif;
+
+    }
+
+    body{
+        color: #6c7983;
+        font-size: 1.2rem;
+        &:: -webkit-scrollbar{
+            width: 7px;
+        }
+        &:: -webkit-scrollbar-thumb{
+            background-color: #27AE60;
+            border-radius: 10px;
+        }
+        &:: -webkit-scrollbar-track{
+            background-color: #EDEDED;
+        }
+    }
+
+`;
+
+export default GlobalStyle;
